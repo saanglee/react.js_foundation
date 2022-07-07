2ab55987c9125e5eaaeb63382d1b0f8963845415
sangji <sangllee20@gmail.com>
2022-07-07 18:57:00 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import './App.scss'
 function App() {
   return (
     <div className='app'>
-      <header className='appHeader'>
-        <LogoImage className='appLogo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='appLink' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className='appLink' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        Learn React
+      </a>
     </div>
   )
 }
